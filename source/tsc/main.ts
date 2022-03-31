e868d6ab00e1588796f7fcbf775275798f77d988
Elliot Hodge <73705352+Craftidore@users.noreply.github.com>
2022-03-31 12:39:59 -0500

    (Maybe) parse with markedjs

--- source/tsc/main.ts
+++ source/tsc/main.ts
@@ -1,10 +1,23 @@
 addEventListener("DOMContentLoaded", (event) => {
+	// set marked settings
+	marked.use({
+		pedantic: false,
+		gfm: true,
+		breaks: false,
+		sanitize: false,
+		smartLists: true,
+		smartypants: false,
+		xhtml: false
+	});
+
+	// render hash is necessary
 	if (location.hash != "" && location.hash != "#") {
 		loadBlog(location.hash)
 	}
 	else {
 		loadBlog("#Home")
 	}
+
 });
 addEventListener("hashchange", (event) => {
 	loadBlog(location.hash)
@@ -17,20 +30,8 @@ function loadBlog(hash:string){
 }
 function parseMarkdown(markdownText:string):string {
 	console.log(markdownText)
-	const htmlText = ("\r\n\r\n" + markdownText
-		.replace(/^### (.*$)/gim, '<h3>$1</h3>')
-		.replace(/^## (.*$)/gim, '<h2>$1</h2>')
-		.replace(/^# (.*$)/gim, '<h1>$1</h1>')
-		.replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
-		.replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
-		.replace(/\*(.*)\*/gim, '<i>$1</i>')
-		.replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
-		.replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>") + "\r\n\r\n")
-        .replace(/\r\n\r\n/gim, "</div><div>")
-		.replace(/\n/gim, ' ')
-        .replace(/\<\/div\>/im, "")
-		.replace(/\<div\>$/im, "")
-		console.log(htmlText);
+	const htmlText = marked.parse(markdownText);
+	console.log(htmlText);
 
 
 	return htmlText.trim()
